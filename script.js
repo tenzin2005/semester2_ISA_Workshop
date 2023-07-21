de983bb88480f7c358ea3ee8d258b6f559554ccc
tenzin2005 <127487213+tenzin2005@users.noreply.github.com>
2023-07-21 11:20:37 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-function compute()
-{
-    var p = parseInt(document.getElementById("principal").value);
-    var r = parseFloat(document.getElementById("rate").value);
-    var y = parseInt(document.getElementById("years").value);
-    if (p <= 0) {
-        alert("Enter a positive number");
-        document.getElementById("principal").focus();
-        return;
-    }
-    else {
-        var i = (p * y * r) / 100;
-        var a = p + i;
-        var result = document.getElementById("result");
-        var actualYear = 2023+y;
-        result.innerHTML = "If you deposit $ <mark>" + p + ",</mark> at an interest rate of <mark>" + r + "% </mark>, you will receive an amount of $ <mark>" + a + ",</mark> in the year <mark>" + actualYear + "</mark>";
+window.addEventListener("load",defaultWeather)
 
+async function defaultWeather() {
+    var city = document.getElementById("search-bar").value;
+    var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=Cambridgeshire&appid=98cbffea2ef6d6011f599f33ae87edde`);
+    var data = await response.json();
+    console.log(data);
+    const cityElement = document.getElementById("city");
+    const temperatureElement = document.getElementById("temperature");
+    const descriptionElement = document.getElementById("description");
+    const pressureElement = document.getElementById("pressure");
+    const humidityElement = document.getElementById("humidity");
+    const windElement = document.getElementById("wind");
+    cityElement.innerHTML = `Weather in ${data.name}`;
+    temperatureElement.innerHTML = `${data.main.temp}°C`;
+    descriptionElement.innerHTML = data.weather[0].description;
+    pressureElement.innerHTML = `Pressure: ${data.main.pressure} hPa`;
+    humidityElement.innerHTML = `Humidity: ${data.main.humidity}%`;
+    windElement.innerHTML = `Wind: ${data.wind.speed} km/h`;
+    var weather_icon = document.getElementById("weather-icon");
+    icon = data.weather[0].icon;
+    weather_icon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    }
+
+async function fetchWeather() {
+    var city = document.getElementById("search-bar").value;
+    var response = await fetch(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=98cbffea2ef6d6011f599f33ae87edde`);
+    var data = await response.json();
+    console.log(data);
+    const cityElement = document.getElementById("city");
+    const temperatureElement = document.getElementById("temperature");
+    const descriptionElement = document.getElementById("description");
+    const pressureElement = document.getElementById("pressure");
+    const humidityElement = document.getElementById("humidity");
+    const windElement = document.getElementById("wind");
+    cityElement.innerHTML = `Weather in ${data.name}`;
+    temperatureElement.innerHTML = `${data.main.temp}°C`;
+    descriptionElement.innerHTML = data.weather[0].description;
+    pressureElement.innerHTML = `Pressure: ${data.main.pressure} hPa`;
+    humidityElement.innerHTML = `Humidity: ${data.main.humidity}%`;
+    windElement.innerHTML = `Wind: ${data.wind.speed} km/h`;
+    var weather_icon = document.getElementById("weather-icon");
+    icon = data.weather[0].icon;
+    weather_icon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
     }
     
-
-}
-        
-function updateRate()
-{
-    rateval= document.getElementById("rate").value;
-    document.getElementById("rate_val").innerHTML=rateval;
-    
-}
